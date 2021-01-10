706748b4b944513e00ee30dee535f79203b27158
рублей шесть <kfearsoff@gmail.com>
2021-01-10 22:07:57 +0300

    Basic EFCore configuration
    Added a basic model, a repository, created a connection string, configured EFCore and created an initial migration

---
+++ Application/Models/EFStoreRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class EFStoreRepository : IStoreRepository
+    {
+        private StoreDbContext context;
+        public EFStoreRepository(StoreDbContext context) => this.context = context;
+        public IQueryable<Product> Products => context.Products;
+    }
+}
--- Application/Startup.cs
+++ Application/Startup.cs
@@ -1,6 +1,9 @@
+using Application.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,11 +15,15 @@ namespace Application
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration config) => Configuration = config;
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<StoreDbContext>(opts => opts.UseSqlServer(Configuration["ConnectionStrings:ApplicationConnection"]));
+            services.AddScoped<IStoreRepository, EFStoreRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
